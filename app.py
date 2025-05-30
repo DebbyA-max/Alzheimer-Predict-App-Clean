14e8fb7fcb028ddbdc2f5aec2b419b664fe06065
DebbyA-max <afolamideborah20@gmail.com>
2025-05-30 03:55:27 +0100

    Fix duplicate number_input IDs by adding unique keys

--- app.py
+++ app.py
@@ -7,12 +7,12 @@ model=joblib.load("alzheimers_model.pkl")
 st.title("Alzheimer's Disease Prediction App")
 st.write("Enter the patient data below to predict Alzheimer's Diagnosis.")
 
-FunctionalAssessment=st.number_input("FunctionalAssessment", min_value=0) 
-ADL=st.number_input("Activities of Daily Living score", min_value=0)
-MMSE=st.number_input("Mini-Mental State Examination score", min_value=0.0)
-MemoryComplaints=st.radio(" Presence of memory complaints", ["Yes", "No"])
+FunctionalAssessment=st.number_input("Functional Assessment Score", min_value=0, key="fun_acc_input") 
+ADL=st.number_input("Activities of Daily Living score", min_value=0, key="ADL_input")
+MMSE=st.number_input("Mini-Mental State Examination score", min_value=0.0, key="MMSE_input")
+MemoryComplaints=st.radio(" Presence of memory complaints", ["Yes", "No"], key="mce_input")
 MemoryComplaints_encoded= 1 if MemoryComplaints=="Yes" else 0
-BehavioralProblems=st.radio(" Presence of behavioral problems", ["Yes", "No"])
+BehavioralProblems=st.radio(" Presence of behavioral problems", ["Yes", "No"], key="bpe_input")
 BehavioralProblems_encoded= 1 if BehavioralProblems=="Yes" else 0 
 
 input_data=np.array([[FunctionalAssessment, ADL, MMSE, MemoryComplaints_encoded, BehavioralProblems_encoded]])
@@ -20,27 +20,3 @@ input_data=np.array([[FunctionalAssessment, ADL, MMSE, MemoryComplaints_encoded,
 if st.button("Predict"):
     prediction=model.predict(input_data)[0]
     st.success(f"Prediction:{prediction}")
-
-
-import streamlit as st
-import numpy as np
-import joblib
-
-model=joblib.load("alzheimers_model.pkl")
-st.title("Alzheimer's Disease Prediction App")
-st.write("Enter the patient data below to predict Alzheimer's Diagnosis.")
-
-FunctionalAssessment=st.number_input("FunctionalAssessment", min_value=0) 
-ADL=st.number_input("Activities of Daily Living score", min_value=0)
-MMSE=st.number_input("Mini-Mental State Examination score", min_value=0.0)
-MemoryComplaints=st.radio(" Presence of memory complaints", ["Yes", "No"])
-MemoryComplaints_encoded= 1 if MemoryComplaints=="Yes" else 0
-BehavioralProblems=st.radio(" Presence of behavioral problems", ["Yes", "No"])
-BehavioralProblems_encoded= 1 if BehavioralProblems=="Yes" else 0 
-
-input_data=np.array([[FunctionalAssessment, ADL, MMSE, MemoryComplaints_encoded, BehavioralProblems_encoded]])
-
-if st.button("Predict"):
-    prediction=model.predict(input_data)[0]
-    st.success(f"Prediction:{prediction}")
-dd32992ff335284256ed2752f58921b4a80837a0
